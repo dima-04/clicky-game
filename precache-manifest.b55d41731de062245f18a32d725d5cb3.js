e752b75d68613dd7899359224b481c2d97127ca8
Dima <dima.dibb1@gmail.com>
2020-01-22 23:40:58 -0800

    Updates

---
+++ precache-manifest.b55d41731de062245f18a32d725d5cb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481e0ccf3148f0e1e6c1cf05e97fd60d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "63d3ffd748e76f1aa016",
+    "url": "/clicky-game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3fb57b88d626b791a672",
+    "url": "/clicky-game/static/css/main.880d0311.chunk.css"
+  },
+  {
+    "revision": "63d3ffd748e76f1aa016",
+    "url": "/clicky-game/static/js/2.64f5e606.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clicky-game/static/js/2.64f5e606.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fb57b88d626b791a672",
+    "url": "/clicky-game/static/js/main.a050fbe7.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
